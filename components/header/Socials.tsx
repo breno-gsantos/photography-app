9df7d09f186d57fa1c04cee0f74904e9c74b6552
Breno <brenogiovanniolintho@gmail.com>
2023-11-29 22:18:57 -0300

    Socials Config

--- components/header/Socials.tsx
+++ components/header/Socials.tsx
@@ -1,7 +1,39 @@
+'use client'
+
+import Link from 'next/link';
+import {ImFacebook, ImTwitter, ImInstagram, ImPinterest} from 'react-icons/im';
+
+const socials = [
+    {
+        href: 'https://www.facebook.com',
+        icon: ImFacebook
+    },
+    {
+        href: 'https://www.x.com',
+        icon: ImTwitter
+    },
+    {
+        href: 'https://www.instagram.com',
+        icon: ImInstagram
+    },
+    {
+        href: 'https://www.pinterest.com',
+        icon: ImPinterest
+    },
+] 
+
 export default function Socials () {
     return (
-        <div className="">
-            Socials
-        </div>
+        <>
+            <ul className='flex gap-x-6'>
+                {socials.map(({href, icon: Icon}) => (
+                    <li key={href} className=''>
+                        <Link href={href} target='_blank'>
+                            <Icon size={20} />
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
